16fee5ba9bdf16f5632743dc15bdd17e5eb7582e
SRI ROOPA <80977779+sriroopar@users.noreply.github.com>
2023-05-17 00:04:17 +0530

    Add files via upload

---
+++ Day8/CeaserCipherCodeOptimized.py
@@ -0,0 +1,46 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+
+print(logo)
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def ceaser(text, shift ,direction):
+    output_text= ''
+    if direction == "decode":
+            shift*= -1
+    for letter in text :
+        position = alphabet.index(letter)
+        new_position = position + shift  
+        output_text += alphabet[new_position]
+    print(output_text)
+
+to_be_continued = True
+while to_be_continued:
+     
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    value_shift= int(input("Type the shift number:\n"))
+    shift = value_shift %26
+    ceaser(text,shift,direction)
+    rerun = input("Type 'yes' to continue and 'no' to stop : ")  
+    if rerun =='no':
+        to_be_continued = False
